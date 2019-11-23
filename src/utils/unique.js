34ff420bf6f94d1c2c4c4768d152f2e83e414dfd
Chris <chrisiivix@gmail.com>
2019-11-24 10:47:29 +1100

    Finished function uniqueSpells.

--- src/utils/unique.js
+++ src/utils/unique.js
@@ -1,25 +1,31 @@
-
 const unique = {
-  spells: (items) => uniqueSpells(items)
-}
+  spells: items => uniqueSpells(items)
+};
 
-const uniqueSpells = (spells) => {
-  const names = [];
+const uniqueSpells = spells => {
+  const names = [], classTypes = [], components = [], schools = [], levels = [], castingTimes = [], ranges = [], durations = [];
 
   spells.forEach(spell => {
     names.push(spell.name);
-    classTypes.push(spell);
-    components.push(spell);
-    schools.push(spell);
-    levels.push(spell);
-    castingTimes.push(spell);
-    ranges.push(spell);
-    durations.push(spell);
+    spell.classTypes.forEach(value => classTypes.push(value));
+    spell.components.forEach(value => components.push(value));
+    schools.push(spell.school);
+    levels.push(spell.level);
+    castingTimes.push(spell.castingTime);
+    ranges.push(spell.range);
+    durations.push(spell.duration);
   });
 
-  //let unique = a.filter((item, i, arr) => arr.indexOf(item) === i);
-
-  return names;
-}
+  return (uniqueValues = {
+    names:        names.filter((item, i, arr) => arr.indexOf(item) === i).sort(),
+    classTypes:   classTypes.filter((item, i, arr) => arr.indexOf(item) === i).sort(),
+    components:   components.filter((item, i, arr) => arr.indexOf(item) === i).sort(),
+    schools:      schools.filter((item, i, arr) => arr.indexOf(item) === i).sort(),
+    levels:       levels.filter((item, i, arr) => arr.indexOf(item) === i).sort(),
+    castingTimes: castingTimes.filter((item, i, arr) => arr.indexOf(item) === i).sort(),
+    ranges:       ranges.filter((item, i, arr) => arr.indexOf(item) === i).sort(),
+    durations:    durations.filter((item, i, arr) => arr.indexOf(item) === i).sort()
+  });
+};
 
 module.exports = unique;
