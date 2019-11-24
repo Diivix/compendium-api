ea152e8f5c37fe40720097891fc3360ca86f8a50
Chris <chrisiivix@gmail.com>
2019-11-24 22:24:06 +1100

    Allowed spells to be lighlty loaded.

--- src/routes/spell.js
+++ src/routes/spell.js
@@ -5,29 +5,44 @@ const unique = require('../utils/unique');
 
 //
 // Get all spells
-//
+// [optional] url query param "lightlyload"
 router.get('/', function(req, res) {
-  return db.spells.findAll()
-    .then(spells => {
-      return res.status(200).send(spells);
-    })
-    .catch(err => {
-      debug('Error retrieving spells. %o', JSON.stringify(err));
-      return res.status(500).send("Error retrieving spells.");
-    });
+  //TODO: Check if param exists, even with no value.
+  if (req.query.lightlyload) {
+    return db.spells
+      .findAll({ attributes: { exclude: ['description', 'atHigherLevels', 'reference', 'createdAt', 'updatedAt'] } })
+      .then(spells => {
+        return res.status(200).send(spells);
+      })
+      .catch(err => {
+        debug('Error retrieving spells. %o', JSON.stringify(err));
+        return res.status(500).send('Error retrieving spells.');
+      });
+  } else {
+    return db.spells
+      .findAll()
+      .then(spells => {
+        return res.status(200).send(spells);
+      })
+      .catch(err => {
+        debug('Error retrieving spells. %o', JSON.stringify(err));
+        return res.status(500).send('Error retrieving spells.');
+      });
+  }
 });
 
 //
 // Get unique values of spells
 //
 router.get('/unique', function(req, res) {
-  return db.spells.findAll()
+  return db.spells
+    .findAll()
     .then(spells => {
       return res.status(200).send(unique.spells(spells));
     })
     .catch(err => {
       debug('Error retrieving spells. %o', JSON.stringify(err));
-      return res.status(500).send("Error retrieving spells.");
+      return res.status(500).send('Error retrieving spells.');
     });
 });
 
@@ -37,13 +52,14 @@ router.get('/unique', function(req, res) {
 router.post('/', function(req, res) {
   const query = req.body;
 
-  return db.spells.findAll({where: {}})
+  return db.spells
+    .findAll({ where: {} })
     .then(spells => {
       return res.status(200).send(spells);
     })
     .catch(err => {
       debug('Error retrieving spells. %o', JSON.stringify(err));
-      return res.status(500).send("Error retrieving spells.");
+      return res.status(500).send('Error retrieving spells.');
     });
 });
 
