af1de68f6a88481df9253c859e22445bbbc6b0e1
Chris <chrisiivix@gmail.com>
2020-01-20 23:05:27 +1100

    Updated cors.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-// const cors = require('cors')
+const cors = require('cors')
 const passport = require('passport');
 const debug = require('debug')('server') // debug logger
 const morgan = require('morgan')         // request logger
@@ -13,18 +13,19 @@ const jwtStrategy = require('./utils/auth');
 const app = express();
 debug('booting %o', 'Compendium API');
 
-// const corsWhitelist = process.env.CORS_WHITELIST.split(",");
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (corsWhitelist.indexOf(origin) !== -1 || (process.env.NODE_ENV !== "production" && !origin)) {
-//       callback(null, true)
-//     } else {
-//       callback('Not allowed by CORS')
-//     }
-//   }
-// }
+const corsOptions = {
+  origin: process.env.CORS_WHITELIST,
+  // function (origin, callback) {
+  //   if (corsWhitelist.indexOf(origin) !== -1 || (process.env.NODE_ENV !== "production" && !origin)) {
+  //     callback(null, true)
+  //   } else {
+  //     callback('Not allowed by CORS')
+  //   }
+  // },
+  credentials: true,
+}
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
