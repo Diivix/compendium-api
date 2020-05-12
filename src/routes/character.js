f3be74bb6f2ad8f0ad1516ce302c77eade10fc4e
Chris <chrisiivix@gmail.com>
2020-05-12 10:27:22 +1000

    Updated character delete function.

--- src/routes/character.js
+++ src/routes/character.js
@@ -5,15 +5,15 @@ const debug = require('debug')('route:character'); // debug logger
 //
 // Get all of the user's characters, lightly loaded.
 //
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   const userId = parseInt(req.user.id);
 
   return db.users
     .findByPk(userId)
-    .then(user => {
-      user.getCharacters().then(characters => res.status(200).send(characters));
+    .then((user) => {
+      user.getCharacters().then((characters) => res.status(200).send(characters));
     })
-    .catch(err => {
+    .catch((err) => {
       debug('Error retrieving user and characters. %o', JSON.stringify(err));
       return res.status(500).send('Error retrieving user and characters.');
     });
@@ -22,14 +22,14 @@ router.get('/', function(req, res) {
 //
 // Get a full character by ID
 //
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
   const userId = parseInt(req.user.id);
   const characterId = parseInt(req.params.id);
 
   return db.characters
     .findOne({ where: { id: characterId, userId: userId } })
-    .then(character => res.status(200).send(character))
-    .catch(err => {
+    .then((character) => res.status(200).send(character))
+    .catch((err) => {
       debug('Error retrieving user and characters. %o', JSON.stringify(err));
       return res.status(500).send('Error retrieving user and characters.');
     });
@@ -38,61 +38,65 @@ router.get('/:id', function(req, res) {
 //
 // Create a character
 //
-router.post('/', async function(req, res) {
-  if(!req.body.name) return res.status(200).send("Character name cannot be empty.");
+router.post('/', async function (req, res) {
+  if (!req.body.name) return res.status(200).send('Character name cannot be empty.');
 
   const id = parseInt(req.user.id);
   const { name, level, classType, description } = req.body;
   const date = new Date().toISOString();
   return db.users
     .findByPk(id)
-    .then(user => {
-      db.characters
-        .create({ userId: id, name, level, classType, description, date, date })
-        .then(character => {
-          user.addCharacters(character);
-          res.status(201).send(character);
-        })
+    .then((user) => {
+      db.characters.create({ userId: id, name, level, classType, description, date, date }).then((character) => {
+        user.addCharacters(character);
+        res.status(201).send(character);
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       debug('Error retrieving user and adding characters. %o', JSON.stringify(err));
-      return res.status(500).send("Error retrieving user and adding characters.");
+      return res.status(500).send('Error retrieving user and adding characters.');
     });
 });
-
 
 //
 // Delete a character
 //
-router.delete('/:id/:characterId', function(req, res) {
+router.delete('/:id', async function (req, res) {
   const userId = parseInt(req.user.id);
   const characterId = parseInt(req.params.id);
 
-  return db.users
-    .findByPk(userId)
-    .then(user => {
-      user.removeCharacters(characterId).then(characters => res.status(200).send(characters));
-    })
-    .catch(err => {
-      debug('Error retrieving user and removing characters. %o', JSON.stringify(err));
-      return res.status(500).send('Error retrieving user and removing characters.');
+  const character = await db.characters
+    .findOne({ where: { id: characterId, userId: userId } })
+    .catch((err) => {
+      debug('Could not find character to delete. %o', JSON.stringify(err));
+      return null;
+  });
+
+  if (character === null) return res.status(500).send();
+
+  return db.characters
+    .destroy({ where: { id: characterId, userId: userId } })
+    .then(res.status(200).send())
+    .catch((err) => {
+      debug('Error deleting character. %o', JSON.stringify(err));
+      return res.status(500).send('Could not delete character.');
     });
 });
 
 //
 // Add a spell to a character
 //
-router.put('/:id/spell/:spellId', async function(req, res) {
+router.put('/:id/spell/:spellId', async function (req, res) {
   const userId = parseInt(req.user.id);
   const characterId = parseInt(req.params.id);
   const spellId = parseInt(req.params.spellId);
 
-  const user = await db.users.findByPk(userId).catch(err => {
+  const user = await db.users.findByPk(userId).catch((err) => {
     debug('Error retrieving user. %o', JSON.stringify(err));
     return null;
   });
 
-  const spell = await db.spells.findByPk(spellId).catch(err => {
+  const spell = await db.spells.findByPk(spellId).catch((err) => {
     debug('Error retrieving spell. %o', JSON.stringify(err));
     return null;
   });
@@ -100,18 +104,18 @@ router.put('/:id/spell/:spellId', async function(req, res) {
   if (!user) return res.status(500).send('Error retrieving user.');
   if (!spell) return res.status(500).send('Error retrieving spell.');
 
-  return user.getCharacters({ where: { id: characterId } }).then(characters => {
+  return user.getCharacters({ where: { id: characterId } }).then((characters) => {
     if (characters && characters.length === 1) {
       characters[0]
         .addSpell(spell)
-        .then(result => {
+        .then((result) => {
           if (result) {
             return res.status(201).send(result);
           } else {
             return res.status(204); // 204 - no response, spell already associated.
           }
         })
-        .catch(err => {
+        .catch((err) => {
           debug('Error associating character with spell. %o', JSON.stringify(err));
           return res.status(500).send('Error associating character with spell.');
         });
